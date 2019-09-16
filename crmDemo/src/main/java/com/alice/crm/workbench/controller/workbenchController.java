e498262b0ab76acdc58a32b188778859862d6d80
ShuaichenLau <68754807@qq.com>
2019-09-16 23:49:54 +0800

    login function

---
+++ crmDemo/src/main/java/com/alice/crm/workbench/controller/workbenchController.java
@@ -0,0 +1,47 @@
+package com.alice.crm.workbench.controller;
+
+import com.alice.crm.user.entity.User;
+import com.alice.crm.user.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/workbench")
+public class workbenchController {
+
+    private final Logger logger = LoggerFactory.getLogger(workbenchController.class);
+
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping("/index")
+    public String workbenchIndex(Model model, HttpServletRequest request, HttpServletResponse response) {
+        logger.info("com.alice.crm.workbench.controller.workbenchController.workbenchIndex");
+
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+
+        System.out.println(userName);
+        System.out.println(password);
+
+        try {
+            if (userName != null && password != null) {
+                User byUser = userService.getByUser(userName, password);
+                model.addAttribute("byUser", byUser);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return "workbench/index.html";
+    }
+}
