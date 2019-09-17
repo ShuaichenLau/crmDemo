f919d83a5ad486266ed7c6866b51436c7a991afe
ShuaichenLau <68754807@qq.com>
2019-09-17 19:44:13 +0800

    application run done hint

---
+++ crmDemo/src/main/java/com/alice/crm/commons/AfterServiceStarted.java
@@ -0,0 +1,20 @@
+package com.alice.crm.commons;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class AfterServiceStarted implements ApplicationRunner {
+
+    /**
+     * 会在服务启动完成后立即执行
+     */
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        log.info("Successful service startup!");
+    }
+}
